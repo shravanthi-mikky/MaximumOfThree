a1c6fa90e447499538b8073702d10b3bd4f7f311
shravanthi-mikky <shravanthi27041996@gmail.com>
2022-05-03 12:35:15 +0530

    [shravanthi] Finding maximum of three float values

---
+++ MaxOfThree/MaxOfDouble.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxOfThree
+{
+    public class MaxOfDouble
+    {
+        public double MaximumOfThree(double a, double b, double c)
+        {
+            double max;
+            max = a;
+            if (b.CompareTo(max) > 0)
+                max = b;
+            if (c.CompareTo(max) > 0)
+                max = c;
+            return max;
+
+        }
+    }
+}
