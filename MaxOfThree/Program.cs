afb4cc5ac58f9575c60008a6024d219fb8f719cd
shravanthi-mikky <shravanthi27041996@gmail.com>
2022-05-03 12:09:18 +0530

    [shravanthi] uc1 - maximum of three integers

--- MaxOfThree/Program.cs
+++ MaxOfThree/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Program to find maximum of three!");
+using MaxOfThree;
+
+Console.WriteLine("Program to find maximum of three! \nPlease enter three numbers :");
+int num1 = Convert.ToInt16(Console.ReadLine());
+int num2 = Convert.ToInt16(Console.ReadLine());
+int num3 = Convert.ToInt16(Console.ReadLine());
+UsingInt first = new();
+int result = first.MaximumOfThree(num1,num2,num3);
+Console.WriteLine("The Maximum of the given numbers is :"+result);
---
+++ MaxOfThree/UsingInt.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxOfThree
+{
+    public class UsingInt
+    {
+        public int MaximumOfThree(int a,int b,int c)
+        {
+            int max;
+            max = a;
+            if(b.CompareTo(max) > 0)
+                max = b;
+            if(c.CompareTo(max) > 0)
+                max = c;
+            return max;
+
+        }
+    }
+    
+}
