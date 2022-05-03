57e3093f13d0caf5a58bfc62b3b12fd871e9bc9b
shravanthi-mikky <shravanthi27041996@gmail.com>
2022-05-03 15:47:35 +0530

    [shravanthi] Finding maximum of three string values

--- MaxOfThree/Program.cs
+++ MaxOfThree/Program.cs
@@ -7,7 +7,7 @@ Console.WriteLine("Program to find maximum of three!");
 
 while(true)
 {
-    Console.WriteLine("Please choose the option :\n1)Maximum of Integers \n2)Maximum of three Floating values");
+    Console.WriteLine("Please choose the option :\n1)Maximum of Integers \n2)Maximum of three Floating values\n3)Maximum of Strings ");
     int option = Convert.ToInt16(Console.ReadLine());   
     switch(option)
     {
@@ -28,6 +28,15 @@ while(true)
             MaxOfDouble maxOfDouble = new();
             double result2 = maxOfDouble.MaximumOfThree(num4, num5, num6);
             Console.WriteLine("The Maximum of the given numbers is :" + result2);
+            break;
+        case 3:
+            Console.WriteLine("Please enter three strings :");
+            string a = Console.ReadLine();
+            string b = Console.ReadLine();
+            string c = Console.ReadLine();
+            MaxOf3Strings MaximumOf3 = new MaxOf3Strings();
+            string result4 = MaximumOf3.MaximumOfThre(a, b, c);
+            Console.WriteLine("The result of MaxOf3Strings :" + result4);
             break;
         default:
             Console.WriteLine("Please choose correct option!");
